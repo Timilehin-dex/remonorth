742a8cadc8089fda52fdd476214e1960a8ee5d58
Timilehin <timiboi14@gmail.com>
2024-11-04 07:49:16 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkGreen: "hsl(140, 32%, 22%)",
+      },
+    },
+  },
+  plugins: [],
+};
